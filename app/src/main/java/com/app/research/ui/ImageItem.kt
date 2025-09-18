25f637eb51f97e2a97fa59f78133ddae68d86d61
DeepakTiwari-Iroid <deepak.iroidsolutions@gmail.com>
2025-09-18 17:56:03 +0530

    minor import issue

--- app/src/main/java/com/app/research/ui/ImageItem.kt
+++ app/src/main/java/com/app/research/ui/ImageItem.kt
@@ -1,11 +1,12 @@
 package com.app.research.ui
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Warning
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,7 +14,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
 fun ImageItem(
@@ -58,7 +61,7 @@ fun ImageItem(
             isError -> {
                 Icon(
                     imageVector = Icons.Outlined.Warning,
-                    tint = ripeMango,
+                    tint = Color.Red,
                     contentDescription = "Error loading image",
                     modifier = Modifier.size(36.dp),
                 )
